43cef324a5a3975c10666aaf13ccd777600e75d1
mihalks <jurawlew.mih@yandex.ru>
2022-10-10 18:40:26 +0300

    Nolan2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+engine = create_engine ('postgresql+psycopg2://postgres:example\@localhost/database_name')
 
 app = Flask(__name__)
 
